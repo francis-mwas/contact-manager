f147514f10540d65291a6ba75e023af4b1fdf9fc
francis-mwas <mwangif184@gmail.com>
2019-07-08 14:59:20 +0300

    Updates

---
+++ precache-manifest.c5e2240f0c7232ba42e908e90ddca0b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa8c839325a89a57166132f809663fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a0617edb71a7a1cae01",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "915cd10a79d70d20c814",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2a0617edb71a7a1cae01",
+    "url": "/static/js/2.6e42b7df.chunk.js"
+  },
+  {
+    "revision": "915cd10a79d70d20c814",
+    "url": "/static/js/main.7699e9bd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
